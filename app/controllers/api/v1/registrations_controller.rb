a0c7386802de969eb981d1734e724746852dd42b
Ash <ashramsey@gmail.com>
2013-06-24 09:12:18 +0800

    return json objects for session requests

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -13,10 +13,11 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
                       :data => { :user => resource,
                                  :auth_token => current_user.authentication_token } }
     else
-      render :status => :unprocessable_entity,
-             :json => { :success => false,
-                        :info => resource.errors,
-                        :data => {} }
+      render status: :unprocessable_entity, json: { error: { title: t('user.errors.signup.generic.title'), message: resource.errors.first.last } } and return
+      # render :status => :unprocessable_entity,
+      #        :json => { :success => false,
+      #                   :info => resource.errors,
+      #                   :data => {} }
     end
   end
 end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -7,7 +7,25 @@ class Api::V1::SessionsController < Devise::SessionsController
   end
 
   def create
-    warden.authenticate!(:scope => resource_name, :store => false, :recall => "#{controller_path}#failure")
+    # warden.authenticate!(:scope => resource_name, :store => false, :recall => "#{controller_path}#failure")
+    email = params[:user]['email']
+    password = params[:user]['password']
+
+    build_resource
+    resource = User.find_for_database_authentication(:email => email)
+
+    unless resource
+      render status: 401, json: { error: { title: t('user.errors.signin.email_not_found.title'), message: t('user.errors.signin.email_not_found.message', email: email) } } and return
+    end
+
+    unless resource.valid_password?(password)
+      render status: 401, json: { error: { title: t('user.errors.signin.invalid_password.title'), message: t('user.errors.signin.invalid_password.message') } } and return
+    end
+
+    unless resource.active_for_authentication?
+      render status: 401, json: { error: { title: t('user.errors.signin.account_disabled.title'), message: t('user.errors.signin.account_disabled.message') } } and return
+    end
+    
     render :status => 200,
            :json => { :success => true,
                       :info => "Logged in",
@@ -15,7 +33,7 @@ class Api::V1::SessionsController < Devise::SessionsController
   end
 
   def destroy
-    current_user.reset_authentication_token!
+    current_user.try(:reset_authentication_token!)
     render :status => 200,
            :json => { :success => true,
                       :info => "Logged out",
